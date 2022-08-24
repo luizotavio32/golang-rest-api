cd5957dcb0c942b20a37d49afb4cb2c85fe9f468
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-08-24 18:25:35 -0300

    feat: add personalidade controller

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/luizotavio32/golang-rest-api/models"
 	"github.com/luizotavio32/golang-rest-api/routes"
 )
 
 
 func main() {
+
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+	
 	fmt.Println("Inciando servidor rest")
 	routes.HandleRequest()
 }
