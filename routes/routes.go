94ce5141b789192183f974cc0510c5a277bdd758
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-08-24 18:25:17 -0300

    refactor: added gorilla mux

--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,14 @@ package routes
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 	"github.com/luizotavio32/golang-rest-api/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controllers.Home)
-	http.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.Home)
+	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
