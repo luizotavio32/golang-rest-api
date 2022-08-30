559302bb0cc888d1a69d478e6b4b94ae7917da85
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-08-30 19:19:32 -0300

    chore: postfres docker cfg and initial migation

---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,9 @@
+create table personalidades(
+    id serial primary key,
+    nome varchar,
+    historia varchar
+);
+
+INSERT INTO personalidades(nome, historia) VALUES
+('Patrick Bateman', 'Patrick Bateman is a fictional character created by novelist Bret Easton Ellis. He is the villain protagonist and narrator of Ellis 1991 novel American Psycho and is portrayed by Christian Bale in the 2000 film adaptation. He is a wealthy and materialistic yuppie and Wall Street investment banker who leads a secret life as a serial killer. Bateman has also briefly appeared in other Ellis novels and their film and theatrical adaptations.'),
+('Manoel Gomes', 'Cantor brasileiro conhecido pelo hit internacional "caneta azul, azul caneta"');
