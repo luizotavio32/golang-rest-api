e7e491cff88c0df5177211b72aa7321c99c0a6a4
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-08-30 19:48:06 -0300

    feat: add database module call

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/luizotavio32/golang-rest-api/database"
 	"github.com/luizotavio32/golang-rest-api/models"
 	"github.com/luizotavio32/golang-rest-api/routes"
 )
@@ -14,7 +15,7 @@ func main() {
 		{Id:1, Nome: "Nome 1", Historia: "Historia 1"},
 		{Id:2, Nome: "Nome 2", Historia: "Historia 2"},
 	}
-	
+	database.ConectaComBancoDeDados()
 	fmt.Println("Inciando servidor rest")
 	routes.HandleRequest()
 }
