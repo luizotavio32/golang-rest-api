8c52fb4efee8619fcdc09f6f4611f8fd3b9240df
Luiz Otavio <luiz.otavio@2rpnet.com>
2022-08-31 18:09:15 -0300

    feat: add content-type middleware

--- routes/routes.go
+++ routes/routes.go
@@ -6,10 +6,12 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/luizotavio32/golang-rest-api/controllers"
+	"github.com/luizotavio32/golang-rest-api/middleware"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
